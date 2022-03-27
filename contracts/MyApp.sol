bec79b90949aa8c3008db41e324ce3d854e61c30
Altaf Shaikh <75683704+AltafMShaikh@users.noreply.github.com>
2022-03-27 19:07:18 +0530

    Update MyApp.sol

--- contracts/MyApp.sol
+++ contracts/MyApp.sol
@@ -1,5 +1,4 @@
-// pragma solidity >=0.8.0 <0.9.0;
-// SPDX-License-Identifier: MIT
+
 pragma solidity >=0.8.0 <0.9.0;
 
 contract MyApp {
@@ -15,7 +14,6 @@ contract MyApp {
 	*/
 	uint productId = 0;
 
-	// define all custom structs
 
 	struct Manufacturer {
 		bool exists;
@@ -33,38 +31,18 @@ contract MyApp {
 		address[] owners;
 	}
 
-	// struct Customer {
-	// 	bool exists;
-	// 	string name;
-	// 	address _address;
-	// }
+	
+
+	
 
 
-	// mapping(address => Customer) public customers;
-	// mappings to store manufacturers and products informations
-	mapping(address => Manufacturer) public manufacturers;
-	mapping(uint => Product) products;
-
-
-	// events to be emitted when certain operatons are completed
-	event ManufacturerCreated(string name, address _address);
-	event ProductCreated(uint id, address manufacturer);
-	event OwnershipUpdated(uint id, address newOwner);
-
-
-	/*
-		Constructor is called when this contract is deployed on the network.
-		It sets the owner of the contract as the address which deploys it.
-	*/
+	
 	constructor() {
 		owner = msg.sender;
 	}
 
 
-	/*
-		Used to create a new Manufacturer
-		Can be called only by the owner of the contract
-	*/
+	
 	function createManufacturer(string memory _name, address _address) public {
 		require(msg.sender == owner, "Only owner is authorised to create a manufacturer!");
 
@@ -76,10 +54,6 @@ contract MyApp {
 	}
 
 
-	/*
-		Used to create a new product
-		Can be called only by the manufacturer
-	*/
 	function createProduct(string memory _name, string memory _model) public {
 		require(manufacturers[msg.sender].exists == true, "You are not a Manufacturer!");
 
@@ -98,21 +72,12 @@ contract MyApp {
 	}
 
 
-	/*
-		Returns a tuple of a product struct
-		- p.owners[0] is the first owner (manufacturer)
-		- last address in the owners list is the current owner
-	*/
+	
 	function getProduct(uint _id) public view returns(Product memory) {
 		return products[_id];
 	}
 
 
-	/*
-		This function is called when an owner sells a product to new customer.
-		It updates the current owner of the product, and also adds them to owners list.
-		* only the current owner of the product is allowed to sell it
-	*/
 	function updateOwnership(uint _id, address _newOwner) public {
 		Product storage p = products[_id];
 		require(p.curOwner == msg.sender, "Not authorized");
